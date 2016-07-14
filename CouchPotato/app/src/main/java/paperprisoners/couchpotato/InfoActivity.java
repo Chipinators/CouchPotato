997aa390b9ee55370c65a1984f96d98a63714a42
Ian Donovan <epicpunnum@ymail.com>
2016-07-14 02:50:16 -0400

    UI Overhaul Part 1
    Fullscreen Inconsistencies fixed! Hurrah my dudes.
    SplashscreenActivity created and now the initializing Activity!
    Paper Prisoners logo redone for good and saved as brand_XXX.png
    InfoActivity now has actual content (albiet placeholder stuff)!
    SetupDialog now features a working player ListView and LOADING SPINNERS!!!
    KickDialog added to message players about well...getting kicked.
    Start of Wouldchuck fragments for use in the GameActivity class.
    Various size tweaks, color changes, text changes.
    Yeeesh, that's a lot.

--- CouchPotato/app/src/main/java/paperprisoners/couchpotato/InfoActivity.java
+++ CouchPotato/app/src/main/java/paperprisoners/couchpotato/InfoActivity.java
@@ -1,21 +1,37 @@
 package paperprisoners.couchpotato;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class InfoActivity extends AppCompatActivity {
+public class InfoActivity extends Activity implements View.OnClickListener {
+
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Layout setup
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_info);
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView( R.layout.activity_info );
+        back = (Button) findViewById( R.id.info_back );
+        back.setOnClickListener( this );
     }
 
+    @Override
+    public void onClick(View v) {
+        if ( v == back ) {
+            Intent toSelect = new Intent( this, GameSelectActivity.class );
+            this.startActivity( toSelect );
+        }
+    }
 }
---
+++ CouchPotato/app/src/main/java/paperprisoners/couchpotato/SetupAdapter.java
@@ -0,0 +1,50 @@
+package paperprisoners.couchpotato;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Ian on 7/13/2016.
+ */
+public class SetupAdapter extends ArrayAdapter<UserData> {
+
+    private boolean isHost;
+    private LayoutInflater mInflater;
+
+    public SetupAdapter(Context context, int resource, boolean isHost) {
+        super(context, resource);
+        this.isHost = isHost;
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Retrieve data
+        UserData item = getItem(position);
+        // Use layout file to generate View
+        View view = mInflater.inflate(R.layout.item_setup, null);
+        // Set user name
+        TextView name;
+        name = (TextView) view.findViewById(R.id.item_setup_name);
+        name.setText(item.username);
+        // Set comment
+        TextView device;
+        device = (TextView) view.findViewById(R.id.item_setup_device);
+        if (item.device != null)
+            device.setText(item.device.getName());
+        else
+            device.setText( getContext().getString(R.string.na_device) );
+        //Toggles button
+        if (!isHost) {
+            RelativeLayout layout;
+            layout = (RelativeLayout) view.findViewById(R.id.item_setup_content);
+            layout.removeView(view.findViewById(R.id.item_setup_kick));
+        }
+        return view;
+    }
+}
